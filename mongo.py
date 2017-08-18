f348f7f284ab0ad10575d383111a519ae06a01a1
Manwholikespie <rdominguez@me.com>
2017-08-18 14:45:04 -0500

    Change !name and !whoami to use mongo instead of pickle file. I also made it `import mongo` instead of `from mongo import \*` I really wish there was an easier way for me to test this stuff on my laptop first. I don't think there were really any errors, though.

--- mongo.py
+++ mongo.py
@@ -3,10 +3,7 @@ from pprint import pprint
 client = MongoClient('mongodb://localhost:27017')
 db = client.sue
 
-def clean(inputString):
-    return inputString.strip().lower()
-
-# ========================   INTERFACE WITH MONGODB   ==========================
+### !DEFINE
 def findDefn(defnName):
     q = db.defns.find_one({'name' : defnName})
     return q
@@ -16,4 +13,12 @@ def addDefn(defnName, meaning):
 
 def updateDefn(defnName, meaning):
     db.defns.update_one({'name' : defnName}, {'$set' : {'meaning' : meaning}})
-# ======================   END INTERFACE WITH MONGODB   ========================
+
+### !NAME, !WHOAMI
+def findName(phoneNumber):
+    """searches for a given phone number in our names collection, and updates """
+    q = db.names.find_one({'phonenumber' : phoneNumber})
+    return q['name']
+
+def updateName(phoneNumber,newName):
+    db.names.update_one({'phoneNumber':phoneNumber}, {'$set' : {'name' : newName}}, upsert=True)
