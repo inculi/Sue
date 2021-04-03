a1c4337deff5433149926fe3afb8c06149860881
Robert <rdominguez@me.com>
2021-04-03 00:28:43 -0700

    Update packages (hex, npm)

--- apps/sue_web/mix.exs
+++ apps/sue_web/mix.exs
@@ -38,11 +38,11 @@ defmodule SueWeb.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.5.3"},
-      {:phoenix_live_view, "~> 0.13.0"},
+      {:phoenix_live_view, "~> 0.15.0"},
       {:floki, ">= 0.0.0", only: :test},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.0"},
+      {:phoenix_live_dashboard, "~> 0.4"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
