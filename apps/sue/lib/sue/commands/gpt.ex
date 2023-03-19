1c2bfbccd64bdc01196ca7c29b40d3ac5fc071ab
Robert Dominguez <rdominguez@me.com>
2023-03-19 18:43:09 -0500

    Update GPT without args

--- apps/sue/lib/sue/commands/gpt.ex
+++ apps/sue/lib/sue/commands/gpt.ex
@@ -5,6 +5,10 @@ defmodule Sue.Commands.Gpt do
   Asks ChatGPT a question.
   Usage: !gpt write a poem about a bot named sue
   """
+  def c_gpt(%Message{args: ""}) do
+    %Response{body: "Please provide a request to ChatGPT. See !help gpt"}
+  end
+
   def c_gpt(%Message{args: args}) do
     %Response{body: Sue.AI.chat_completion(args)}
   end
