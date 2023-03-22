bd5c01a6942729a32716ea5e77e2e205a812ca5b
Robert Dominguez <rdominguez@me.com>
2023-03-22 00:27:14 -0500

    send messages in discord

--- apps/sue/lib/sue/mailbox/discord.ex
+++ apps/sue/lib/sue/mailbox/discord.ex
@@ -11,7 +11,6 @@ defmodule Sue.Mailbox.Discord do
   end
 
   def handle_event({:MESSAGE_CREATE, dmsg, _ws_state}) do
-    # Logger.debug(dmsg |> inspect(pretty: true))
     msg = Message.from_discord(dmsg)
     Sue.process_messages([msg])
   end
@@ -20,12 +19,37 @@ defmodule Sue.Mailbox.Discord do
     :noop
   end
 
-  @spec send_response(Message.t(), Response.t()) :: any()
-  def send_response(msg, response) do
-    with {:ok, _} <- Api.create_message(msg.metadata.channel_id, response.body) do
+  def send_response(_msg, %Response{body: nil, attachments: []}) do
+    :ok
+  end
+
+  def send_response(msg, %Response{attachments: []} = rsp) do
+    # No attachments
+    send_response_text(msg, rsp)
+  end
+
+  def send_response(msg, %Response{body: nil, attachments: atts}) do
+    # No text
+    send_response_attachments(msg, atts)
+  end
+
+  def send_response(%Message{} = msg, %Response{attachments: atts} = rsp) do
+    send_response_text(msg, rsp)
+    send_response_attachments(msg, atts)
+  end
+
+  def send_response_text(msg, rsp) do
+    with {:ok, _} <- Api.create_message(msg.metadata.channel_id, rsp.body) do
       :ok
     else
       error -> Logger.error(error |> inspect())
     end
+  end
+
+  def send_response_attachments(_, []), do: :ok
+
+  def send_response_attachments(msg, [att | atts]) do
+    Api.create_message(msg.metadata.channel_id, file: att.filename)
+    send_response_attachments(msg, atts)
   end
 end
