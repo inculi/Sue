1c10a5747fb90c9810eccf57fb04c2f5e3dcbeea
Robert Dominguez <rdominguez@me.com>
2023-03-22 12:00:08 -0500

    Update README, disable nostrum runtime autostart

--- apps/sue/lib/sue/application.ex
+++ apps/sue/lib/sue/application.ex
@@ -39,8 +39,14 @@ defmodule Sue.Application do
     children_discord =
       if Sue.Utils.contains?(@platforms, :discord) do
         [
+          %{
+            id: Nostrum.Application,
+            start: {Nostrum.Application, :start, [:normal, []]}
+          },
           Sue.Mailbox.Discord
         ]
+      else
+        []
       end
 
     opts = [strategy: :one_for_one, name: Sue.Supervisor]
