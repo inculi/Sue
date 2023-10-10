b4b73d44d145b80bde16a632e6b5f314d49ef2e9
Robert Dominguez <rdominguez@me.com>
2023-10-09 19:46:37 -0700

    Verified it works, changing config back

--- config/config.exs
+++ config/config.exs
@@ -69,7 +69,7 @@ config :phoenix, :json_library, Jason
 config :sue,
   # options include :discord, :imessage, :telegram - :debug is just for testing
   # platforms: [:debug, :discord, :imessage, :telegram],
-  platforms: [:debug, :telegram],
+  platforms: [:debug, :discord, :imessage, :telegram],
   chat_db_path: Path.join(System.user_home(), "Library/Messages/chat.db")
 
 config :subaru,
